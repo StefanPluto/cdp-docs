cd101c636e1625d5486d424147db857d0ea878d8
Stefan Ristic <stefanristic@Stefans-MacBook-Pro.local>
2023-10-09 11:34:46 +0200

    Initial StopChurn documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+      }}
+    >
+      <img
+        style={{ width: "60px" }}
+        src="https://raffle-image-storage.s3.eu-central-1.amazonaws.com//ChurnStopLogo-removebg-preview.png"
+      />
+      <h2>StopChurn</h2>
+    </div>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+};
 
-export default config
+export default config;
